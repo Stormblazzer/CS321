87c0a34ffd770f27a54229a32fdd35c81c30c2cd
Ethan Bridges <ethanbridges190@gmail.com>
2023-10-23 23:54:59 -0400

    Iteration 3
    Uploading the tests and objects made in iteration 3

---
+++ tests/Iteration3/GreenReplacementCardBO.java
@@ -0,0 +1,85 @@
+package iteration3;
+
+import java.util.Date;
+import java.util.Locale;
+
+public class GreenReplacementCardBO {
+    private int id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private Date dateOfBirth;
+    private String countryOfBirth;
+    private HomeAddress homeAddress;
+    private String reasonForReplacement;
+
+    public GreenReplacementCardBO(int id, String firstName, String middleName, String lastName, Date dateOfBirth, String countryOfBirth, HomeAddress homeAddress, String reasonForReplacement) {
+
+    }
+
+    /**
+     * Getter & Setter methods
+     */
+    public void setId(int id){
+    }
+    public int getId(){
+        return -1;
+    }
+    public void setFirstName(String firstName){
+    }
+    public String getFirstName(){
+        return null;
+    }
+    public void setMiddleName(String middleName){
+    }
+    public String getMiddleName(){
+        return null;
+    }
+    public void setLastName(String lastName){
+
+    }
+    public String getLastName(){
+        return null;
+    }
+    public void setDateOfBirth(Date dateOfBirth){
+
+    }
+    public Date getDateOfBirth(){
+        return null;
+    }
+    public void setCountryOfBirth(String countryOfBirth){
+    }
+    public Date getCountryOfBirth(){
+        return null;
+    }
+    public void setHomeAddress(HomeAddress homeAddress){
+
+    }
+    public String getHomeAddress(){
+        return null;
+    }
+    public void setReasonForReplacement(String reasonForReplacement){
+
+    }
+    public String getReasonForReplacement(){
+        return null;
+    }
+
+    /**
+     * Validates country of birth exists
+     * @param countryOfBirth
+     * @return bool
+     */
+    public Boolean isValidCountry(String countryOfBirth){
+        return false;
+    }
+
+    /**
+     * Retrieves form with matching form id.
+     * @param formId
+     * @return GreenReplacementCardBO
+     */
+    public GreenReplacementCardBO getForm(int formId){
+        return null;
+    }
+}
---
+++ tests/Iteration3/HomeAddress.java
@@ -0,0 +1,36 @@
+package iteration3;
+
+public class HomeAddress {
+    private String streetName;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public HomeAddress(String streetName, String city, String state, String zipCode){
+        this.streetName = streetName;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+    public void setStreetName(String streetName){
+
+    }
+    public String getStreetName(){
+        return null;
+    }
+    public void setCity(String city){
+
+    }
+    public String getCity(){
+        return null;
+    }
+    public void setState(String state){
+
+    }
+    public String getState(){
+        return null;
+    }
+    public void setZipCode(String zipCode){
+        
+    }
+}
